813ce998bf69f077d0e4ecfd9d22a5a4b03c5ef4
Chrisruberto <69993756+Chrisruberto@users.noreply.github.com>
2023-05-06 22:52:15 -0300

    Add files via upload

---
+++ MEA/mea/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Marca, MEA, Cliente, Pedido
+# Register your models here.
+
+class Admin(admin.ModelAdmin):
+    list_display = ['id','marca','nombre_producto', 'cantidad', 'peso_neto', 'precio', 'codigo' ]
+    list_filter = ['marca',]
+    search_fields = ['nombre_producto']
+        
+class AdminC(admin.ModelAdmin):
+    list_display = ['nombre','telefono']
+
+class AdminP(admin.ModelAdmin):
+    list_display = ['cliente','fecha','pago','nota']
+    search_fields = ['cliente']
+
+admin.site.register(Marca)
+admin.site.register(MEA,Admin)
+admin.site.register(Cliente,AdminC)
+admin.site.register(Pedido,AdminP)
---
+++ MEA/mea/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+# Create your models here.
+
+class Relacion(models.Model):
+    nombre = models.CharField(max_length=150)
+    
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return self.nombre 
+    
+class Marca(Relacion):
+    
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+        
+class MEA(models.Model):
+    nombre_producto = models.CharField(max_length=150, verbose_name='Nombre del Producto')
+    marca = models.ForeignKey(Marca, verbose_name='Marca', on_delete=models.CASCADE)
+    cantidad = models.IntegerField( verbose_name='Cantidad Disponible')
+    peso_neto = models.CharField(max_length=50, verbose_name='Peso neto, ml/gr')
+    codigo = models.BigIntegerField( verbose_name='Código')
+    precio = models.DecimalField(max_digits=10, decimal_places=2,  verbose_name='Precio')
+
+    class Meta:
+        verbose_name = 'MEA'
+        verbose_name_plural = 'MEA'
+
+    def __str__(self):
+        return str(self.nombre_producto)
+    
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=150, verbose_name='Nombre del Cliente')
+    telefono = models.CharField(max_length=150)
+    
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+
+    def __str__(self):
+        return str(self.nombre)
+    
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, verbose_name='Cliente', on_delete=models.CASCADE)
+    productos = models.ManyToManyField(MEA, verbose_name=('Prodcutos'))
+    pago = models.BooleanField(default=True)
+    nota = models.CharField(max_length=150)
+    fecha = models.DateTimeField(auto_created=True,verbose_name='Fecha del pedido')
+    
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+
+    def __str__(self):
+        return str(self.cliente)
+    
+    
